b1a7dfddb70777f193de5d12cbf970fe3704cb60
user1 <user1@kimsw>
2019-01-08 20:15:40 +0900

    zzzz

--- FinalProject/src/main/java/com/spring/bnb/controller/SWController.java
+++ FinalProject/src/main/java/com/spring/bnb/controller/SWController.java
@@ -5,16 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HYController {
-	
-	@RequestMapping(value = "/index.air", method = RequestMethod.GET)
+public class SWController {
+
+	@RequestMapping(value = "/list.air", method = RequestMethod.GET)
 	public String index() {
 		return "home/homeDetail.hometiles";
 	}
 	
-	@RequestMapping(value = "/apiTest.air", method = RequestMethod.GET)
-	public String apiTest() {
-		return "apiTest.notiles";
-	} 
 }
-
