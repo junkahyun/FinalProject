6b4b5eeec09e55c4a59ea8e60faba5fd856579f1
user1 <user1@kimwc>
2019-01-08 20:30:37 +0900

    Kwc

--- FinalProject/src/main/java/com/spring/bnb/controller/KwcController.java
+++ FinalProject/src/main/java/com/spring/bnb/controller/KwcController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.spring.bnb.service.InterHYService;
+import com.spring.bnb.service.*;
 import com.spring.common.AES256;
 
 @Controller
@@ -13,7 +13,7 @@ public class KwcController {
 	
 	//===== #35. ������ü �����ϱ�(DI:Dependency Injection)  =====
 	@Autowired
-	private InterHYService service; // �������̽��� ��õ� �޼ҵ常 ��밡���ϴ�.
+	private InterKwcService service; // �������̽��� ��õ� �޼ҵ常 ��밡���ϴ�.
 	
 	// 45. ����� ��ȣȭ �˰����� AES256�� ����Ͽ� ��ȣȭ/��ȣȭ �ϱ� ���� Ŭ���� ������ü �����ϱ�(DI:Dependency Injection)
 	@Autowired
--- FinalProject/src/main/java/com/spring/bnb/service/KwcService.java
+++ FinalProject/src/main/java/com/spring/bnb/service/KwcService.java
@@ -3,12 +3,12 @@ package com.spring.bnb.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.spring.bnb.dao.InterHYDAO;
+import com.spring.bnb.dao.*;
 
 //===== #31. Service ����  =====
 @Service
 public class KwcService implements InterHYService {
 	//===== #34. ������ü �����ϱ�(DI:Dependency Injection)  =====
 	@Autowired
-	private InterHYDAO dao; // �������̽��� ��õ� �޼ҵ常 ��밡���ϴ�.
+	private InterKwcDAO dao; // �������̽��� ��õ� �޼ҵ常 ��밡���ϴ�.
 }
