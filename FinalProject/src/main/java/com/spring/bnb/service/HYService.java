a2e75e5be6a54d319592163264f92b048662a0a7
Hyun0JAM <42133544+Hyun0JAM@users.noreply.github.com>
2019-01-08 20:06:07 +0900

    190108

--- FinalProject/src/main/java/com/spring/bnb/service/HYService.java
+++ FinalProject/src/main/java/com/spring/bnb/service/HYService.java
@@ -1,5 +1,14 @@
 package com.spring.bnb.service;
 
-public class HYService implements InterHYService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.spring.bnb.dao.InterHYDAO;
+
+//===== #31. Service 선언  =====
+@Service
+public class HYService implements InterHYService {
+	//===== #34. 의존객체 주입하기(DI:Dependency Injection)  =====
+	@Autowired
+	private InterHYDAO dao; // 인터페이스에 명시된 메소드만 사용가능하다.
 }
