840ed7f41b2b8163107fb75a1bf8d6c237b93064
dhbae <dhbae@DESKTOP-H4M3I7C>
2019-01-09 14:35:10 +0900

    2019.01.09

--- FinalProject/src/main/java/com/spring/bnb/dao/SCDAO.java
+++ FinalProject/src/main/java/com/spring/bnb/dao/SCDAO.java
@@ -2,8 +2,11 @@ package com.spring.bnb.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class SCDAO implements InterSCDAO {
+	
 	@Autowired
 	private SqlSessionTemplate sqlsession; // SqlSessionTemplate --> root-context.xml  #15.
 }
