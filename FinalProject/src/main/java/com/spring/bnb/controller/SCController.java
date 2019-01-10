aa913a997743c260db8c534e8457ac2a45038781
user1 <user1@Suwook>
2019-01-10 10:23:41 +0900

    20190110_수욱

--- FinalProject/src/main/java/com/spring/bnb/controller/SCController.java
+++ FinalProject/src/main/java/com/spring/bnb/controller/SCController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.bnb.service.InterHYService;
+import com.spring.bnb.service.InterSWService;
 
 @Controller
 public class SCController {
 	
 	@Autowired
-	private InterHYService service;
+	private InterSWService service;
 	
 	
 }
