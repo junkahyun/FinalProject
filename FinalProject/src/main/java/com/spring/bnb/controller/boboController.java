8254aa38d1a48902606197b88bf2a7c21da351f1
qhgusdld77 <43057657+qhgusdld77@users.noreply.github.com>
2019-01-24 17:55:21 +0900

    2019-01-24 보현

--- FinalProject/src/main/java/com/spring/bnb/controller/boboController.java
+++ FinalProject/src/main/java/com/spring/bnb/controller/boboController.java
@@ -140,9 +140,24 @@ public class boboController {
 		return "become-host/room-step2-page.hosttiles_nofooter";
 	}
 	
-	@RequestMapping(value="/roomstep3.air", method={RequestMethod.GET})
-	public String roomstep3(RoomVO roomvo, HttpServletRequest request) {
+	// 드롭하는 이미지 파일명 가져오기
+	@RequestMapping(value="/dropJOSN.air", method={RequestMethod.GET})
+	public String dropJOSN(HttpServletRequest req) {
 		
+		String fileName = req.getParameter("fileName");
+		JSONArray jsonArr = new JSONArray();
+		
+		System.out.println(fileName);
+		jsonArr.put(fileName);
+		
+		String str_json = jsonArr.toString();
+		req.setAttribute("str_json", str_json);
+		
+		return "JSON";
+	}
+	
+	@RequestMapping(value="/roomstep3.air", method={RequestMethod.GET})
+	public String roomstep3(RoomVO roomvo, HttpServletRequest request) {	
 		return "become-host/room-step3.hosttiles_nofooter";
 	}
 	
@@ -159,70 +174,5 @@ public class boboController {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-
-	
-	@RequestMapping(value="/roomimage.air", method={RequestMethod.GET})
-	public String roomimage() {
-		
-		return "become-host/roomimage.hosttiles_nofooter";
-	}
-	
-	
-	
-	
-	
-	
-	
-    @RequestMapping(value = "/fileUpload/post") //ajax에서 호출하는 부분
-    @ResponseBody
-    public String upload(MultipartHttpServletRequest multipartRequest) { //Multipart로 받는다.
-          
-        Iterator<String> itr =  multipartRequest.getFileNames();
-         
-        String filePath = "C:/test"; //설정파일로 뺀다.
-         
-        while (itr.hasNext()) { //받은 파일들을 모두 돌린다.
-             
-             //기존 주석처리
-         /*   MultipartFile mpff = multipartRequest.getFile(itr.next());
-            String originFileName = mpff.getOriginalFilename();
-            System.out.println("FILE_INFO: "+originFileName); //받은 파일 리스트 출력'
-*/           
-             
-            MultipartFile mpf = multipartRequest.getFile(itr.next());
-      
-            String originalFilename = mpf.getOriginalFilename(); //파일명
-      
-            String fileFullPath = filePath+"/"+originalFilename; //파일 전체 경로
-      
-            try {
-                //파일 저장
-                mpf.transferTo(new File(fileFullPath)); //파일저장 실제로는 service에서 처리
-                 
-                System.out.println("originalFilename => "+originalFilename);
-                System.out.println("fileFullPath => "+fileFullPath);
-      
-            } catch (Exception e) {
-                System.out.println("postTempFile_ERROR======>"+fileFullPath);
-                e.printStackTrace();
-            }
-                          
-       }
-          
-        return "success";
-    }
-	
-	
- 
-    
-    
-    
-	
-	
 }
+ 
