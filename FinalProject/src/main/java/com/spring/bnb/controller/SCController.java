0429ef13b27591b7f38ddb4b2ad8f8841f261b11
dhbae <dhbae@DESKTOP-H4M3I7C>
2019-01-25 17:58:10 +0900

    2019.01.25 승철백

--- FinalProject/src/main/java/com/spring/bnb/controller/SCController.java
+++ FinalProject/src/main/java/com/spring/bnb/controller/SCController.java
@@ -18,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.spring.bnb.model.RoomVO;
 import com.spring.bnb.service.InterHYService;
