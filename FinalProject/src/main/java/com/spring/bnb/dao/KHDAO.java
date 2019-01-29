cb58516ec473d8dda06235b59d9017cf3e7b1e27
user1 <user1@kahyuns>
2019-01-29 09:19:38 +0900

    2019.01.29

--- FinalProject/src/main/java/com/spring/bnb/dao/KHDAO.java
+++ FinalProject/src/main/java/com/spring/bnb/dao/KHDAO.java
@@ -16,14 +16,6 @@ public class KHDAO implements InterKHDAO {
 
 	@Autowired
 	private SqlSessionTemplate sqlsession;
-
-	// *** 숙소 이용규칙 가져오는 메소드 *** //
-	@Override
-	public List<HashMap<String,String>> getRoomRule() {
-		List<HashMap<String,String>> roomRule = sqlsession.selectList("kh.getRoomRule");
-		return roomRule;
-	}
-	
     //======================================================================
 	// *** 숙소 정보 뽑아오는 메소드 *** //
 	@Override
