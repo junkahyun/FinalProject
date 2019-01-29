bdc2e4fc1aa1fa569de4d507c0cca6812575e2c8
dhbae <dhbae@DESKTOP-H4M3I7C>
2019-01-29 09:19:57 +0900

    2019.01.29 승철백

--- FinalProject/src/main/java/com/spring/bnb/controller/KHController.java
+++ FinalProject/src/main/java/com/spring/bnb/controller/KHController.java
@@ -250,42 +250,6 @@ public class KHController {
 	      return "O"+year+month+day+"-"+ordcode;
    }
 	
-	///////////////////////////////////////////////////////////////////////////////////
-	
-	// ***** 호스트 등록된 숙소 수정하기(기본요금 수정) ***** //
-	@RequestMapping(value="/changeDefaultRoomCharge.air", method= {RequestMethod.GET})
-	public String changeDefaultRoomCharge (HttpServletRequest req) {
-		return "hostRoomEdit/changeDefaultRoomCharge.hosttiles_nofooter";
-	}
-	
-	// ***** 호스트 등록된 숙소 수정하기(추가요금 수정) ***** //
-	@RequestMapping(value="/changePlusRoomCharge.air", method= {RequestMethod.GET})
-	public String changePlusRoomCharge (HttpServletRequest req) {
-		return "hostRoomEdit/changePlusRoomCharge.hosttiles_nofooter";
-	}
-	
-	// ***** 호스트 등록된 숙소 수정하기(체크인과 체크아웃 수정) ***** //
-	@RequestMapping(value="/changeCheckInCheckOut.air", method= {RequestMethod.GET})
-	public String changeCheckInCheckOut (HttpServletRequest req) {
-		return "hostRoomEdit/changeCheckInCheckOut.hosttiles_nofooter";
-	}
 
-	// ***** 호스트 등록된 숙소 수정하기(숙박인원 수정) ***** //
-	@RequestMapping(value="/changeHostingPeople.air", method= {RequestMethod.GET})
-	public String changeHostingPeople (HttpServletRequest req) {
-		return "hostRoomEdit/changeHostingPeople.hosttiles_nofooter";
-	}
-
-	// ***** 호스트 등록된 숙소 수정하기(편의시설 및 이용규칙 수정) ***** //
-	@RequestMapping(value="/changeConvenienceAndRule.air", method= {RequestMethod.GET})
-	public String changeConvenienceAndRule (HttpServletRequest req) {
-		
-		// ** 이용규칙 가져오는 메소드 ** //
-		List<HashMap<String,String>> roomRule = service.getRoomRule();
-		
-		req.setAttribute("roomRule", roomRule);
-		
-		return "hostRoomEdit/changeConvenienceAndRule.hosttiles_nofooter";
-	}
 
 }
